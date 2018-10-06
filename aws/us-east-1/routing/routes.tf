7b8cd46a45a8df60acb73c6e03dfd00799511bbc
Atlas Kerr <atlaskerr@gmail.com>
2018-10-06 18:48:18 -0500

    Simplified output naming scheme

--- aws/us-east-1/routing/routes.tf
+++ aws/us-east-1/routing/routes.tf
@@ -1,7 +1,11 @@
-output "admin_vpn_subnet_us_east_1b" {
+output "igw" {
+  value = "192.168.1.0/24"
+}
+
+output "admin_vpn_us_east_1b" {
   value = "192.168.2.0/24"
 }
 
-output "git_subnet_us_east_1c" {
+output "git_us_east_1c" {
   value = "192.168.3.0/24"
 }
