b6bfa8ba89c39e1f2c8f92ba0050d960b236de2a
Atlas Kerr <atlaskerr@gmail.com>
2018-10-06 19:07:11 -0500

    separate terraform and provider logic

--- aws/us-east-1/vpc/provider.tf
+++ aws/us-east-1/vpc/provider.tf
@@ -1,11 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "showseeker-terraform"
-    key    = "aws/us-east-1/vpc/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
 }
