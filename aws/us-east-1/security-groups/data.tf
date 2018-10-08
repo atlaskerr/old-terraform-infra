a91bc6bd4eddf894afa9558b110e65985d6a2a5e
Atlas Kerr <atlaskerr@gmail.com>
2018-10-08 01:26:42 -0500

    Remove unused files

--- aws/us-east-1/security-groups/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "main" {
-  backend = "s3"
-
-  config {
-    bucket = "showseeker-terraform"
-    key    = "aws/us-east-1/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- aws/us-east-1/security-groups/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "showseeker-terraform"
-    key    = "aws/us-east-1/security-groups/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
