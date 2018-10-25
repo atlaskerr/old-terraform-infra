1b66260e90098c6783e32e6d03c551e52604bc43
Atlas Kerr <atlaskerr@gmail.com>
2018-10-24 23:46:21 -0500

    services: remove notarysigner and server

--- aws/us-east-1/services/notary/notaryserver/us-east-1c/terraform.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "showseeker-terraform"
-    key    = "aws/us-east-1/services/notary/notary/us-east-1c/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
